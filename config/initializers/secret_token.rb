b90e1c8d6f84310fabd6afd57c1ef39f6a5e46c0
Gabríel A. Pétursson <gabriel@system.is>
2011-09-04 02:24:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deildu::Application.config.secret_token = 'b97242de3b113ef396085dc39a43d6f05dd55dd00365faf5d16f434aed4e1640b67415f03d70b75a679736840b4860eee1841434bf5c5554be83d027ecefa44c'
