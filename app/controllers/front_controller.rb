3c5dc790a0abf3d566ca1e2616831db830ded0c2
Gabríel A. Pétursson <gabriel@system.is>
2011-10-11 21:09:28 +0000

    Fixes #55.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,5 +1,6 @@
 class FrontController < ApplicationController
   def index
     @news = News.order("created_at desc")
+    @active_users = User.where{visited_at > 20.minutes.ago}.order{username.asc}.all
   end
 end
